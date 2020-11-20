b0099061d31c34bfff6275cad0a239ca4630c004
kaleighspitzer <spitzer4@illinois.edu>
2020-11-19 21:18:15 -0600

    Reading in csv file

---
+++ include/core/load_data.h
@@ -0,0 +1,15 @@
+//
+// Created by Kaleigh Spitzer on 11/19/20.
+//
+
+#ifndef FINAL_PROJECT_LOAD_DATA_H
+#define FINAL_PROJECT_LOAD_DATA_H
+
+class Data {
+    public:
+        int main();
+
+    private:
+};
+
+#endif //FINAL_PROJECT_LOAD_DATA_H
--- src/core/load_data.cc
+++ src/core/load_data.cc
@@ -5,12 +5,38 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <vector>
 
-void loadData(std::string fileName) {
-    std::ifstream data("routes.csv");
+std::vector<std::pair<std::string, std::vector<int>>> read_csv() {
+    std::ifstream data("/Users/kaleighspitzer/CLionProjects/~Cinder/my-projects/final-project2/data/routes.csv");
     std::string line;
+    std::string colname;
+    size_t val;
+    std::vector<std::pair<std::string, std::vector<int>>> result;
     while (data.good()) {
-        getline(data, line, ',');
-        std::cout << std::string(line) << std::endl;
+        getline(data, line);
+        std::stringstream ss(line);
+        while (getline(ss, colname, ',')) {
+            result.push_back({colname, std::vector<int> {}});
+        }
     }
+
+    while (getline(data, line)) {
+        std::stringstream ss(line);
+        size_t col_num = 0;
+        while (ss >> val) {
+            result.at(col_num).second.push_back(val);
+            if (ss.peek() == ',') {
+                ss.ignore();
+            }
+            col_num++;
+        }
+    }
+    data.close();
+    return result;
+}
+
+int main() {
+    read_csv();
+    return 0;
 }
