e486e5cfc959f840bc46c6a903f7130ea933bea8
kaleighspitzer <spitzer4@illinois.edu>
2020-12-19 19:27:30 -0600

    Test case for checking route attributes

--- tests/tests_main.cc
+++ tests/tests_main.cc
@@ -11,3 +11,18 @@ TEST_CASE("Number of routes is correct") {
     std::map<std::string, Route> route_id_to_route_map = Data::create_route_map();
     REQUIRE(route_id_to_route_map.size() == 102);
 }
+
+TEST_CASE("None of the file headings are read in") {
+    std::map<std::string, Route> route_id_to_route_map = Data::create_route_map();
+    std::map<std::string, Route>::iterator it = route_id_to_route_map.find("route_id");
+    REQUIRE(it == route_id_to_route_map.end());
+}
+
+TEST_CASE("Route ID attributes are correct") {
+    std::map<std::string, Route> route_id_to_route_map = Data::create_route_map();
+    std::map<std::string, Route>::iterator it = route_id_to_route_map.find("10W GOLD ALT");
+    REQUIRE(it->second.shortName == "10");
+    REQUIRE(it->second.longName == "Gold Alternate");
+    REQUIRE(it->second.color == "c7994a\r");
+    REQUIRE(it->second.trip_id == "[@36.0.28512748@][4][1175208157878]/0__10W_UHS_UMS_SCH\r");
+}
