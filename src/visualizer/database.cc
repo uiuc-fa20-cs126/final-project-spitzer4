ca7a98b016908e25f35c659381ca08741ad6ad1a
kaleighspitzer <spitzer4@illinois.edu>
2021-01-05 11:58:14 -0600

    Added drawString to database

--- src/visualizer/database.cc
+++ src/visualizer/database.cc
@@ -3,6 +3,7 @@
 //
 
 #include "../../include/visualizer/database.h"
+#include "../../include/core/load_data.h"
 
 void database::setup() {
     setWindowSize(600, 800);
@@ -13,10 +14,15 @@ void database::draw() {
     ci::Color(0, 0, 1);
     ci::Rectf rectf(100, 100, 500, 500);
     ci::gl::drawStrokedRect(rectf);
+
+    std::map<std::string, Route> route_map = Data::CreateRouteMap();
+    for (std::map<std::string, Route>::iterator it = route_map.begin(); it != route_map.end(); ++it) {
+        std::string line = it->first + + " " + it->second.longName + " " + it->second.shortName + " " + it->second.color + " " + it->second.trip_id;
+        ci::gl::drawString(line, glm::vec2(700.0f,-150.0f), ci::Color(0, 0, 0), ci::Font("Arial", 90));
+    }
 }
 
 void database::update() {
-
 }
 
 CINDER_APP(database, ci::app::RendererGl)
